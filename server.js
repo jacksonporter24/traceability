d49fef18ab5db65263eb56e96285e8f658f6ee2a
Jackson Porter <readjacksonporter@gmail.com>
2022-07-21 14:37:00 -0600

    fourth push

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
+const { setDefaultResultOrder } = require('dns')
 var rollbar = new Rollbar({
   accessToken: 'e291979a790641bca6320863a8725949',
   captureUncaught: true,
@@ -17,10 +18,54 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-try {
-    nonExistentFunction();
-  } catch (error) {
-    console.error(error);
-    // expected output: ReferenceError: nonExistentFunction is not defined
-    // Note - error messages will vary depending on browser
-  }
+const movies = ['Jurassic Park', 'The Truman Show', 'The Princess Bride']
+
+app.get('/', (req, res) => {
+    rollbar.info("Someone loaded up your html")
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/movies', (req, res) => {
+    rollbar.info("Someone got the list of movies to load.")
+    res.status(200).send(students)
+})
+
+app.post('/api/movies', (req, res) => {
+   let {title} = req.body
+
+
+   const index = movies.findIndex(movie => {
+       return movie === title
+   })
+
+   try {
+       if (index === -1 && title !== '') {
+        rollbar.log("Movie added successfully", {author: "Jackson",
+    type: "manual entry"})
+           movies.push(title)
+           res.status(200).send(movies)
+       } else if (title === ''){
+        rollbar.error("No name h 0.provided.");
+           res.status(400).send('You must enter a name.')
+       } else {
+        rollbar.error("Movie already exists.")
+           res.status(400).send('That movie already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/movies/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    rollbar.info("Movie")
+    res.status(200).send("Movie was deleted.")
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
+
+
