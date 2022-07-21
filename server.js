2c79278049ead23589edb65a62c5b4c1fdbf6935
Jackson Porter <readjacksonporter@gmail.com>
2022-07-21 14:22:27 -0600

    second push

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors());
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'e291979a790641bca6320863a8725949',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  }
